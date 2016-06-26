6e8bf3e0abf1412eb428a014a40d9544324e4a81
colin <colinladley@hotmail.com>
2016-06-26 17:46:32 +0100

    edit view is visible when clicked

--- assets/src/js/components/UrlList.js
+++ assets/src/js/components/UrlList.js
@@ -5,13 +5,70 @@ import ReactDOM from 'react-dom';
 let UrlItem = React.createClass({
     getInitialState() {
         return {
-            url: this.props.url
+            url: this.props.url,
+            edit: false,
+            delete: false
         };
     },
+    componentDidUpdate(){
+        var input = ReactDOM.findDOMNode(this.refs.urlInput);
+        if(input){
+            input.focus();
+        }
+    },
+    onEdit(event) {
+        event.stopPropagation();
+
+        this.setState({
+            edit: true
+        });
+    },
+
+    onEditClose(event) {
+        this.setState({
+            edit: false
+        });
+        window.removeEventListener('click', this.onEditClose);
+    },
+
+    onChange(event) {
+    
+    },
+
+    createEditView() {
+        return (
+            <div className="url-item url-item--edit">
+                <input ref="urlInput" type="text" value={this.state.url} onChange={this.onChange}/>
+            </div>
+        );
+    },
+    createDeleteView() {
+
+    },
+    createDefaultView() {
+        return (
+            <div className="url-item">
+                { this.state.url } 
+                <button className="btnEdit" onClick={this.onEdit}>edit</button>
+                <button className="btnDelete">delete</button>
+            </div>
+        );
+    },
+
 
     render() {
         let url  = this.state.url;
-        let view = <div className="url-item">{ url }</div>;
+        let view;
+
+        if(this.state.edit) {
+            window.addEventListener('click', this.onEditClose, false);
+            view = this.createEditView();
+        }else {
+            view = this.createDefaultView();
+        }
+
+
+
 
 
         return (
